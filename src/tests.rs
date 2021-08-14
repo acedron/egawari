528daf4a7641938c0a7ef9352043c5e872c35308
acedron <acedrons@yahoo.co.jp>
2021-08-14 13:46:38 +0300

    style: remove unnecessary newlines

--- src/tests.rs
+++ src/tests.rs
@@ -4,9 +4,9 @@ use crate::config;
 #[test]
 fn config_file() {
     let mut conf = config::get_config().unwrap();
-    let mut conf_copy = config::get_config().unwrap();
     conf.input.name = String::from("SynPS/2 Synaptics TouchPad");
-    conf_copy.input.name = String::from("SynPS/2 Synaptics TouchPad");
     config::save_config(&conf).unwrap();
-    assert_eq!(config::get_config().unwrap().input.name, conf_copy.input.name);
+
+    let input_name = &conf.input.name;
+    assert_eq!(config::get_config().unwrap().input.name, *input_name);
 }
